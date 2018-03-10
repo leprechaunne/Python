49ba18a3b171f2d126055362200e37b86bacd080
leprechaunne <williamneverly@gmail.com>
2018-03-10 03:35:56 -0500

    Going to bed, N2S: finish transferring concrete classes to entities.py

---
+++ Python Arcade (Snake Clone)/entities.py
@@ -0,0 +1,13 @@
+""" Contains Snake and Apple """
+
+class Snake(object):
+	"""User controlled object that consists of squares.
+	
+	Color can be passed with Snake(color)
+	Snake's main properties are its location_list, length, and score."""
+
+	length - how many 
+
+
+	def __init__(self, arg):
+		self.length
